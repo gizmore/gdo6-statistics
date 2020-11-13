aeacf8c6131925c39be4724f733fef0171876360
gizmore <gizmore@wechall.net>
2020-11-13 07:46:08 +0100

    6.10-r9375_onInitSidebar

--- Module_Statistics.php
+++ Module_Statistics.php
@@ -4,27 +4,32 @@ namespace GDO\Statistics;
 use GDO\Core\GDO_Module;
 use GDO\Core\Method;
 use GDO\DB\GDT_Checkbox;
-use GDO\UI\GDT_Bar;
 use GDO\UI\GDT_Tooltip;
+use GDO\UI\GDT_Page;
 
+/**
+ * Gather statistics about usage of modules and methods.
+ * @author gizmore
+ * @version 6.10
+ * @since 6.08
+ */
 final class Module_Statistics extends GDO_Module
 {
 	public function getClasses()
 	{
-		return array(
-			"GDO\\Statistics\\GDO_Statistic",
-		);
+	    return [
+	        GDO_Statistic::class,
+	    ];
 	}
 	
 	public function onLoadLanguage() { return $this->loadLanguage('lang/statistics'); }
 	
 	public function getConfig()
 	{
-		return array(
+		return [
 			GDT_Checkbox::make('statistics_bottombar')->initial('1'),
-		);
+		];
 	}
-	
 	public function cfgBottomBar() { return $this->getConfigValue('statistics_bottombar'); }
 	
 	public function hookBeforeExecute(Method $method)
@@ -35,10 +40,11 @@ final class Module_Statistics extends GDO_Module
 		}
 	}
 	
-	public function hookBottomBar(GDT_Bar $bar)
+	public function onInitSidebar()
 	{
 		if ($this->cfgBottomBar())
 		{
+		    $bar = GDT_Page::$INSTANCE->bottomNav;
 			$total = GDO_Statistic::totalHits();
 			$today = GDO_Statistic::todayHits();
 			$bar->addField(GDT_Tooltip::make()->icon('trophy')->tooltip('statistics_hitcounter', [$total, $today]));
